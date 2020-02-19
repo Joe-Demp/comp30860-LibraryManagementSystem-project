48653d0e2bab8730240c7e6cf9d92be4a8b52c4a
Joe_Demp <dempseyjoseph96@yahoo.com>
2020-02-19 13:53:43 +0000

    Added an Artifact class.
    Created a package structure for the project. Minor adjustments to the pom.xml.
    
    Signed-off-by: Joe-Demp <dempseyjoseph96@yahoo.com>

---
+++ src/main/java/ie/ucd/DoHO/model/Artifact.java
@@ -0,0 +1,94 @@
+package ie.ucd.DoHO.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
+
+/**
+ * @author Department of Homeland Obscurity
+ *
+ * Class to model common attributes of all Artifacts
+ */
+@Entity
+public abstract class Artifact implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column private String title;
+    @Column private String author;
+    @Column private String publisher;
+    @Column private Date releaseDate;
+    @Column private String subject;
+    @Column private String genre;
+    @Column private String libraryLocation;
+    @Column private String language;
+
+    public Artifact() {
+    }
+
+    /**
+     * An overloaded constructor to facilitate adding Artifacts to the LMS
+     */
+    public Artifact(String title, String author, String publisher, Date releaseDate,
+                    String subject, String genre, String libraryLocation, String language) {
+        setTitle(title);
+        setAuthor(author);
+        setPublisher(publisher);
+        setReleaseDate(releaseDate);
+        setSubject(subject);
+        setGenre(genre);
+        setLibraryLocation(libraryLocation);
+        setLanguage(language);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getPublisher() {
+        return publisher;
+    }
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    public String getLibraryLocation() {
+        return libraryLocation;
+    }
+    public void setLibraryLocation(String libraryLocation) {
+        this.libraryLocation = libraryLocation;
+    }
+    public String getLanguage() {
+        return language;
+    }
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
--- src/test/java/ie/ucd/DoHO/application/DemoApplicationTests.java
+++ src/test/java/ie/ucd/DoHO/application/DemoApplicationTests.java
@@ -1,12 +1,13 @@
-package com.example.demo;
+package ie.ucd.DoHO.application;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
