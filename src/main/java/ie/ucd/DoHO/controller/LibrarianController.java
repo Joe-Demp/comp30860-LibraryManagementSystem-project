c04e2377e0d85392664ed23f73c4f50851eb39bb
Joe_Demp <dempseyjoseph96@yahoo.com>
2020-03-04 22:09:36 +0000

    /create endpoint partially implemented to create artifacts
    Currently accepts books but does not store them. Other Artifacts not yet implemented.
    
    Signed-off-by: Joe-Demp <dempseyjoseph96@yahoo.com>

--- src/main/java/ie/ucd/DoHO/controller/LibrarianController.java
+++ src/main/java/ie/ucd/DoHO/controller/LibrarianController.java
@@ -1,15 +1,15 @@
 package ie.ucd.DoHO.controller;
 
 import ie.ucd.DoHO.model.ArtifactRepository;
+import ie.ucd.DoHO.model.Book;
 import ie.ucd.DoHO.model.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -27,10 +27,36 @@ public class LibrarianController {
     private ArtifactRepository artifactRepository;
     private Logger logger = LoggerFactory.getLogger(LibrarianController.class);
 
+    @ModelAttribute
+    public void addAttributes(Model model) {
+        model.addAttribute("user", userSession.getUser());
+    }
+
     @GetMapping("/portal")
     public String portal() {
         if (userSession.isAdmin()) {
             return "librarian_portal";
+        }
+        return "login_main";
+    }
+
+    @GetMapping("/create")
+    public String addArtifact(Model model) {
+        if (userSession.isAdmin()) {
+            model.addAttribute("book", new Book());
+            // todo add all attributes (all types of artifact)
+            return "add_artifact";
+        }
+        return "login_main";
+    }
+
+    @PostMapping("/create/book")
+    public String addBook(@ModelAttribute Book book, BindingResult errors,
+                          Model model, HttpServletResponse response)
+            throws IOException {
+        if (userSession.isAdmin()) {
+            logger.info("Book received:" + book.toString());
+            response.sendRedirect("/portal");
         }
         return "login_main";
     }
